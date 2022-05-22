ace5c255b490eb398dccd85cb9fde64d9739b34b
cvanbeck <98055697+cvanbeck@users.noreply.github.com>
2022-05-22 23:55:05 +0100

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,5 @@
+function couponCounter(newspaperID) {
+    const numberOfCoupons = document.getElementById(newspaperID).value;
+    console.log(numberOfCoupons)
+    console.log(numberOfCoupons * 1.5)
+}

9891da4a34771770b050c82235c12c30d93f5d28
cvanbeck <98055697+cvanbeck@users.noreply.github.com>
2022-05-23 00:29:48 +0100

    Add function to multiply amount of coupons by their price

--- javascript.js
+++ javascript.js
@@ -1,5 +1,8 @@
-function couponCounter(newspaperID) {
-    const numberOfCoupons = document.getElementById(newspaperID).value;
-    console.log(numberOfCoupons)
-    console.log(numberOfCoupons * 1.5)
+// Need to learn about arrays? to store both paperID and paperPrice
+// for each newspaper we sell
+function couponMultiplier(paperID, paperPrice) {
+    const numberOfCoupons = document.getElementById(paperID).value;
+    const couponsTotal = numberOfCoupons * paperPrice
+    console.log(couponsTotal)
+    return couponsTotal
 }

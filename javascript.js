55595a2f7bf183df626a1ba0736ceb47d9bfc584
cvanbeck <98055697+cvanbeck@users.noreply.github.com>
2022-05-23 03:11:17 +0100

    Value of coupons will console.log automatically

--- javascript.js
+++ javascript.js
@@ -1,8 +1,34 @@
-// Need to learn about arrays? to store both paperID and paperPrice
-// for each newspaper we sell
+
 function couponMultiplier(paperID, paperPrice) {
     const numberOfCoupons = document.getElementById(paperID).value;
     const couponsTotal = numberOfCoupons * paperPrice
-    console.log(couponsTotal)
-    return couponsTotal
+    if (couponsTotal > 0){
+        return couponsTotal.toFixed(2) 
+    }
+    else {
+        return 0
+    }
+
 }
+
+// Not sure if using objects in arrays is the way to go. I think a better way would be for the function at the bottom
+// to use the id to find the price., maybe just an array for each one is better then?
+
+//let telegraphTotal = document.getElementById("telegraph-total")
+// let theTelegraph = {id: "telegraph", price:1.50}
+// let theTimes = {id: "times", price:0.80}
+// let theGuardian = {id: "guardian", price: 2.00}
+// let dailyMail = {id:"dailymail", price: 3.00}
+// let newspaperList = [theTelegraph, theTimes, theGuardian, dailyMail]
+
+
+allPapers = document.querySelectorAll(".input-field")
+allPapers.forEach((allPapers) => {
+    allPapers.addEventListener("input", () => {
+        console.log("INPUT RECEIVED!" + allPapers.id)
+        console.log(couponMultiplier(allPapers.id, ))
+    })
+})
+
+
+// next step is to find a way to incorporate the price of each paper into couponMultiplier

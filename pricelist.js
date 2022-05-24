22b181eb192fb7e32bdb74b9723d31a906aa34f2
cvanbeck <98055697+cvanbeck@users.noreply.github.com>
2022-05-24 14:25:18 +0100

    Update pricelist

--- pricelist.js
+++ pricelist.js
@@ -1,13 +1,23 @@
 let priceList = {
-    "telegraph": 1.50,
-    "times": 0.80,
-    "guardian": 2.00,
-    "dailymail": 3.00
+    "observer": 1.40,
+    "guardian": 2.20,
+    "i": 0.70,
+    "dailymail": 0.80,
+    "mirror": 1.00,
+    "star":0.60,
+    "sun": 0.70,
+    "telegraph": 2.50,
+    "times": 2.20,
 }
 
 let couponTotalPriceList = { 
-    "telegraph": 0.00, 
-    "times": 0.00, 
-    "guardian": 0.00, 
-    "dailymail": 0.00 
+    "observer": 0.00,
+    "guardian": 0.00,
+    "i": 0.00,
+    "dailymail": 0.00,
+    "mirror": 0.00,
+    "star":0.00,
+    "sun": 0.00,
+    "telegraph": 0.00,
+    "times": 0.00,
 }
